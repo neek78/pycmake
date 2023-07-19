1ec8f58090529ead12c354f205152edc764402ad
Nick Begg <nickbegg@gmail.com>
2023-07-19 13:45:32 +0200

    add class doc for kwarg

--- PyModules/cmake/kwarg.py
+++ PyModules/cmake/kwarg.py
@@ -1,5 +1,9 @@
 
 class KwArg:
+    """
+    When calling cmake functions that use cmake_parse_arguments(), passing a
+    KwArg will render into just its name for True, and nothing for False.
+    """
     def __init__(self, enabled : bool = True):
         self._enabled = enabled
 
